cd12e97643614b120da4f0e790dfb9e43cb0ddd7
llouliLafuente <141773015+llouliLafuente@users.noreply.github.com>
2024-04-19 09:36:46 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("ingrese un año: "))
+    if año %400 == 0 or (año %100 !=0 and año %4 == 0):
+        print("el año " + str(año) + " es bisisto")
+    else:
+        print("el año " + str(año) + " no es bisisto")
+
+leap_year()
